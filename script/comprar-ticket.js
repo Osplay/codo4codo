f3d988caf85df158d47f604717bb068f9491ac47
unknown <efollin@protonmail.com>
2023-10-18 20:36:07 -0300

    compras-ticket

---
+++ script/comprar-ticket.js
@@ -0,0 +1,41 @@
+document.getElementById("cantidad").oninput = function() {updateCantidad()};
+document.getElementById("categoria").onchange = function() {updateCantidad()};
+document.getElementById("borrar").onclick = function() {clickBorrar()};
+document.getElementById("resumen").onclick = function() {clickResumen()};
+
+function updateCantidad(){
+
+    var porcentaje = 1;
+    var cantidad = 0;
+
+    cantidad = document.getElementById("cantidad").value;
+    porcentaje = document.getElementById("categoria").value;
+
+    switch(porcentaje){
+        case "0":
+            porcentaje = 0.8;
+            break;
+        case "1":
+            porcentaje = 0.5;
+            break;
+        case "2":
+            porcentaje = 0.15;
+            break;
+        default:
+            porcentaje = 1;
+            break;
+    }
+ 
+    document.getElementById("total").innerHTML = cantidad * porcentaje;
+
+}
+
+function clickBorrar(){
+    document.getElementById("cantidad").value = 0;
+    document.getElementById("categoria").value = 0;
+    document.getElementById("total").innerHTML = 0;
+}
+
+function clickResumen(){
+    updateCantidad(); // F
+}
